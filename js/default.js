29e7efc2b1a7271b0b916fe60415be46f2faae70
Ravuth Yo <ravuthz@gmail.com>
2015-07-10 22:59:53 +0700

    update

--- js/default.js
+++ js/default.js
@@ -4,7 +4,7 @@ console.info('default.js loaded.');
 ;(function($) {
     $(function(){
         $('#adajaxmenu .menu a').on('click', function(){
-            $('#adajaxmenu li').fadeToggle('fast', function(){
+            $('#adajaxmenu li').slideToggle('fast', function(){
                 $('#adajaxmenu .menu').show();
             });
         });
