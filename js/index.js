d2d33ba3eb1627e2831b540592da4c6f5f25cacc
Ravuth Yo <ravuthz@gmail.com>
2015-07-11 23:45:51 +0700

    update

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ console.info('single.js loaded.');
 
 ;(function($){
 	$(function(){
-		var blog_id = '5615873936899142487', max_posts = 9;
+		var blog_id = '5615873936899142487', 
+			max_posts = 9,
+			cpage = 1;
         var url = 'https://www.blogger.com/feeds/' + blog_id +'/posts/default?alt=json&max-results=' + max_posts + '&orderby=published';
         ajaxGet(url, function(data){
             var x = listPosts(data),
@@ -15,6 +17,10 @@ console.info('single.js loaded.');
 
 
 	});
+
+	window.showpageCount = functions(json){
+		console.log('json', json);
+	}
 
 
 
