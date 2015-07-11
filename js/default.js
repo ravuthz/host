fdd0093c766d174f2331038ecd64d6793e710d86
Ravuth Yo <ravuthz@gmail.com>
2015-07-11 15:09:52 +0700

    update

--- js/default.js
+++ js/default.js
@@ -1,39 +1,5 @@
 console.info('default.js loaded.');
 
-
-
-/*(function(){
-    var scripts = ['jquery.bpopup', 'default'];
-    var located = "https://rawgit.com/ravuthz/host/master/js/";
-
-    function loadScript(url){
-        var date = new Date(),
-            scripts = document.createElement("script");
-
-        scripts.type = "text/javascript";
-        scripts.src = url + '.js?' + date.getUTCMilliseconds();
-        document.head.appendChild(scripts);
-    }
-    function loadStyle(url){
-        var date = new Date(),
-            style = document.createElement("link");
-
-        style.type = "text/css";
-        style.rel= "stylesheet";
-        style.href = url + '.css?' + date.getUTCMilliseconds();
-        document.head.appendChild(style);
-    }
-
-    loadStyle('default');
-    loadStyle('respone');
-
-    for(var i=0; i<scripts.length; i++){
-        loadScript(located + scripts[i]);
-    }
-    console.log('loaded all scripts completed.');
-})();*/
-
-
 ;(function($) {
     $(function(){
         $('#adajaxmenu .menu a').on('click', function(){
@@ -41,14 +7,15 @@ console.info('default.js loaded.');
         });
 
         $('.body-post span').each(function(){
-            // var postTags = makePost({
-            //     'title': x,
-            //     'link': y,
-            //     'date': t,
-            //     'comment' : u,
-            //     'content': '...'
-            // });
-            // $(this).html(postTags);
+            var postTags = makePost({
+                'title': x,
+                'link': y,
+                'date': t,
+                'comment' : u,
+                'content': '...'
+            });
+            $(this).html(postTags);
+            $(this).next().remove();
             // $(this).siblings('script').remove();
             $(this).show();
         });
